020bd50d79f1d71e96989a143917c74a4078bc78
Lovestrant Kemboi <71724387+Lovestrant@users.noreply.github.com>
2022-05-19 18:13:35 +0300

    Source port

--- src/index.java
+++ src/index.java
@@ -1,5 +1,23 @@
 public class index {
     public static void main(String[] args) {
+        byte[] bytes = new byte[]{
+                0x13, 0x55, (byte) 0xb6, 0x76,
+                0x79, (byte) 0x88, 0x29, 0x5e,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x03, 0x00, 0x34,
+                0x7e, 0x58, 0x1e, 0x36,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00
+
+        };
+
+
+        int part1 = bytes[0]&0xff; // 0001 0011 & 1011 0110
+        int part2 = bytes[1]&0xff;
+
+       String SourcePort = part1+""+part2;
+        System.out.println("SourcePort: " +SourcePort);
+
 
     }
 }
